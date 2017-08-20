577d2b7cc5c681676d2cbc429ded1c32ce467182
wagan <wagan@172.17.4.1>
2017-08-20 23:39:46 +0800

    complete pp, jd todo

--- src/ali.py
+++ src/ali.py
@@ -1,9 +1,13 @@
+# -*- coding: utf-8 -*-
 '''
 @author: wagan
 '''
 import urllib.request
 import json
 import re
+import copy
+from elasticsearch import Elasticsearch
+
 
 def fetchMobile():
     url="https://www.taobao.com/market/3c/shouji.php"
@@ -27,30 +31,102 @@ def readAliJson():
                 if key in item["tip"]:
                     print(parseWords(item["tip"]))
 
-def searchProduct():
+def searchPpProduct():
     url="https://mstore.ppdai.com/product/searchPro"
     pageIndex = 1;
     pageSize = 100
     while True:
         payload ={"pageIndex":pageIndex, "pageSize":pageSize,"name":""}
-        params = json.dumps(payload).encode('utf8')
-        request = urllib.request.Request(url, data=params,
-                                 headers={'content-type': 'application/json;charset=UTF-8', "Accept": "application/json"})
-        response = urllib.request.urlopen(request)
-        responseContent = response.read()
-        proContent = json.loads(responseContent)
+        proContent = getReponseFromPp(url, payload)
         totalPage = proContent["responseContent"]["totalPage"]
         prodList = proContent["responseContent"]["product004List"]
         for prod in prodList:
             prod["name"] = re.sub(u"(\u2018|\u2019|\xa0|\u2122)", "", prod["name"])
             del prod["pictureUrl"],prod["iconTypeName"]
+            prodId = prod["linkUrl"][9:]
+            ppProdSku(prodId, prod)
+            #insertEs(prod)
         print(prodList)
         if pageIndex >= totalPage:
             break
         pageIndex = pageIndex + 1
     pass
 
-def jd():
+
+def ppProdSku(prodId, prod):
+    url = "https://mstore.ppdai.com/product/getAttribute"
+    payload = {"productSkuId": prodId}
+    proContent = getReponseFromPp(url, payload)
+    
+    crossIds=[]
+    attributeNames={}
+    attributeValueIds = {}
+    init = True
+    for attributes in proContent["responseContent"]:
+        preKeeyIds = crossIds
+        crossIds=[]
+        preIds = copy.deepcopy(preKeeyIds)
+        for attribute in attributes["attributeList"]:
+            attributeValueIds[attribute["attributeValueId"]] = attributes["attributeId"]
+            attributeNames[attribute["attributeValueId"]] = {"name": attribute["attributeValue"], "attributeId": attributes["attributeId"]}
+            if init:
+                crossIds.append([attribute["attributeValueId"]])
+                continue
+            for ids in preIds:
+                copyIds = copy.deepcopy(ids)
+                attributeValueId = attribute["attributeValueId"]
+                copyIds.append(attributeValueId)
+                crossIds.append(copyIds) 
+        init = False
+    
+    prodName = prod["name"]
+    srcProdName = getDefaultName(crossIds, attributeNames, attributeValueIds, prodName)
+    for attributeIds in crossIds:
+        exist = True
+        prodName = srcProdName
+        for attributeValueId in attributeIds:
+            attributeName = attributeNames[attributeValueId]["name"]
+            if attributeName not in prodName:
+                exist = False
+                prodName = prodName.replace("{" + str(attributeValueIds[attributeValueId]) + "}", attributeName)
+        if not exist:
+            print(prodName, getPrice(attributeIds, prodId)) 
+        
+def getDefaultName(crossIds, attributeNames, attributeValueIds, prodName):
+    defaultName = prodName
+    for attributeIds in crossIds:
+        for attributeValueId in attributeIds:
+            attributeName = attributeNames[attributeValueId]["name"]
+            if attributeName in prodName:
+                defaultName = defaultName.replace(attributeName, "{" + str(attributeValueIds[attributeValueId]) + "}")    
+    return defaultName
+    
+
+def getPrice(attributeIds, prodId):
+    url = "https://mstore.ppdai.com/product/getSkuProSimpleByAttr"
+    payload = {"attributeValueIds":attributeIds, "productSkuId": prodId}
+    proContent = getReponseFromPp(url, payload)
+    return proContent["responseContent"]["price"]
+    
+
+def getReponseFromPp(url, payload):
+    params = json.dumps(payload).encode('utf8')
+    request = urllib.request.Request(url, data=params,
+                                headers={'content-type': 'application/json;charset=UTF-8', "Accept": "application/json"})
+    response = urllib.request.urlopen(request)
+    responseContent = response.read()
+    return json.loads(responseContent) 
+
+
+es = Elasticsearch(hosts=[{'host': "172.17.4.203", 'port': 9200}])
+def insertEs(doc):
+    es.index(index="pp", doc_type='prod', body=doc)
+    
+
+
+def jd(key):
+    url="https://so.m.jd.com/ware/search.action?keyword=Apple%20iPhone%207%20Plus"
+    
     pass
 
 def parseWords(tip):
@@ -77,5 +153,21 @@ def parseWords(tip):
     return words
 
 if __name__ == '__main__':
-    searchProduct()
+    searchPpProduct()
+    results = es.search(index="pp", doc_type='prod', body=  {
+        'query': {
+            'match': {
+                'name':  '32G'
+            }
+        },
+        "highlight" : {
+            "pre_tags" : ["<tag1>", "<tag2>"],
+            "post_tags" : ["</tag1>", "</tag2>"],
+            "fields" : {
+                "content" : {}
+            }
+        }
+    })
+    for result in results["hits"]["hits"]:
+        print(result)
     pass
